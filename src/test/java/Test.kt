f656aae9df56081ce01fc6e878fdacefc1e6427d
peon <binifiska@gmail.com>
2021-09-02 00:09:22 +0300

    initial commit

--- src/test/java/Test.kt
+++ src/test/java/Test.kt
@@ -1,27 +1,52 @@
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import java.net.Inet4Address
 
 
 fun main() {
-    var ia = Inet4Address.getByName("128.65.22.228")
-    ia = Inet4Address.getByName("142.250.186.46")
-    //ia = Inet4Address.getLocalHost()
-    if (ia.isReachable(3000)) {
-        print(ia.hostAddress)
+    CoroutineScope(Dispatchers.Main).launch {
+        println(ping("128.65.22.228"))
+        delay(10)
     }
+    CoroutineScope(Dispatchers.Main).launch {
+        println(ping("1.1.1.1"))
+        delay(10)
+    }
+    CoroutineScope(Dispatchers.Main).launch {
+        println(ping("127.0.0.1"))
+        delay(10)
+    }
+    CoroutineScope(Dispatchers.Main).launch {
+        println(ping("142.250.186.46"))
+        delay(10)
+    }
+
+
+
+
+}
+
+fun ping(host: String): Boolean {
+    //var host = "128.65.22.228"
+    //host = "142.250.186.46"
 
     val command = "ping"
-    val host = "128.65.22.228"
-    val attributes = arrayOf("-c 1", "-s 32")
-    val pb = ProcessBuilder(command, host, *attributes)
-    val process = pb.start()
+    val attributes = arrayOf("-c 1", "-s 32") //LINUX
+    val processBuilder = ProcessBuilder(command, host, *attributes)
+    val process = processBuilder.start()
     val reader = BufferedReader(InputStreamReader(process.inputStream))
+    val stringBuilder = StringBuilder()
     var line: String?
     while (reader.readLine().also { line = it } != null) {
-        println(line)
+        stringBuilder.appendLine(line)
     }
-
-
-
+    //print(stringBuilder.toString())
+    var isReachable = false
+    if (stringBuilder.contains(Regex("="))) {
+        isReachable = true
+    }
+    return isReachable
 }
