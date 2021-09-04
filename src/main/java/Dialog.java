1c8fc7d7bbd3e73a058d1781cac836e9cead0e7e
peon <binifiska@gmail.com>
2021-09-04 13:07:22 +0300

    initial commit

---
+++ src/main/java/Dialog.java
@@ -0,0 +1,75 @@
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.io.File;
+import java.util.Arrays;
+
+public class Dialog extends JDialog {
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+    private JPanel panelOKCancelButtons;
+    private JTable tableMainHostsInfo;
+    private JButton buttonLoadHosts;
+    private JButton buttonCheckAllHostsConnection;
+
+    private JFileChooser fileChooser;
+    private DefaultTableModel tableModel;
+
+    private MainLogicSolution mainLogicSolution = new MainLogicSolution();
+
+    public Dialog() {
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
+
+        panelOKCancelButtons.setVisible(false);
+        initModelForJTable();
+
+
+        buttonLoadHosts.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                fileChooser = new JFileChooser();
+                fileChooser.setFileFilter(new FileNameExtensionFilter("Excel files", "xlsx"));
+                int isFileSelectedInt = fileChooser.showOpenDialog(contentPane);
+                if (isFileSelectedInt == JFileChooser.APPROVE_OPTION) {
+                    File selectedFile = fileChooser.getSelectedFile();
+                    mainLogicSolution.loadInfo(selectedFile);
+                    String[][] mainHostsInfo = mainLogicSolution.getHostsInfo();
+                    for (String[] row : mainHostsInfo) {
+                        Object[] dataRowForTable = new Object[mainHostsInfo[0].length];
+                        for (int i = 0; i < row.length; i++) {
+                            dataRowForTable[i] = row[i];
+                        }
+                        tableModel.addRow(dataRowForTable);
+                    }
+                }
+            }
+        });
+
+        buttonCheckAllHostsConnection.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mainLogicSolution.checkAllHostsConnection();
+            }
+        });
+    }
+
+    private void initModelForJTable() {
+        tableModel = new DefaultTableModel();
+        tableModel.addColumn("Hosts");
+        tableModel.addColumn("Status");
+        tableMainHostsInfo.setModel(tableModel);
+    }
+
+    public static void main(String[] args) {
+        Dialog dialog = new Dialog();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }
+}
---
+++ src/main/java/MainLogicSolution.kt
@@ -0,0 +1,64 @@
+import com.sun.xml.internal.fastinfoset.util.StringArray
+import org.apache.poi.ss.usermodel.WorkbookFactory
+import java.io.File
+import java.io.FileInputStream
+
+class MainLogicSolution {
+    private val mainHostsInfo: ArrayList<ArrayList<String>> = arrayListOf()
+    private var hostColumnNumber = -1
+
+    private val connectionChecker = ConnectionChecker()
+
+    fun getHostsInfo(): Array<Array<String>> {
+        val arrayList = arrayListOf<Array<String>>()
+        mainHostsInfo.forEach { arrayList.add(it.toTypedArray()) }
+        return arrayList.toTypedArray()
+    }
+
+    fun loadInfo(file: File) {
+        val fileInputStream = FileInputStream(file)
+        val workbook = WorkbookFactory.create(fileInputStream)
+        val sheet = workbook.getSheetAt(0);
+        fileInputStream.close()
+
+        var contentSheetLineNumberPosition = 0
+        while (sheet.getRow(contentSheetLineNumberPosition) == null) contentSheetLineNumberPosition++
+        val titleLineColumns: ArrayList<String> = arrayListOf()
+        var sheetColumnNumberPosition = 0
+        while (sheet.getRow(contentSheetLineNumberPosition).getCell(sheetColumnNumberPosition) != null) {
+            titleLineColumns.add(sheet.getRow(contentSheetLineNumberPosition).getCell(sheetColumnNumberPosition).stringCellValue)
+            sheetColumnNumberPosition++
+        }
+        for (index in titleLineColumns.indices) {
+            if (titleLineColumns[index] == HOST_COLUMN_NAME) {
+                hostColumnNumber = index
+                break
+            }
+        }
+        contentSheetLineNumberPosition++
+        val columnsCount = titleLineColumns.size
+        while (sheet.getRow(contentSheetLineNumberPosition) != null) {
+            val lineInfo: ArrayList<String> = arrayListOf()
+            for (index in 0 until columnsCount) {
+                lineInfo.add(sheet.getRow(contentSheetLineNumberPosition).getCell(index).stringCellValue)
+            }
+            lineInfo.add("") //FOR STATUS INFO
+            mainHostsInfo.add(lineInfo)
+            contentSheetLineNumberPosition++
+        }
+    }
+
+    fun checkAllHostsConnection() {
+        if (hostColumnNumber == -1) return
+        val hosts = arrayListOf<String>()
+        mainHostsInfo.forEach {
+            hosts.add(it[hostColumnNumber])
+        }
+        connectionChecker.loadHosts(hosts)
+        connectionChecker.checkAllHostsConnection()
+    }
+
+    companion object {
+        const val HOST_COLUMN_NAME = "host"
+    }
+}
